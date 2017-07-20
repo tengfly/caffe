0ec2752a30b9b93a71a2d3a7f9a2e57fa2dd3055
Ke Chen <keche@microsoft.com>
2017-07-20 04:04:26 +0000

    Add new api interface for caffe prediction (based on the example code classification.cpp).

---
+++ include/caffe/prediction.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#ifdef USE_OPENCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#endif  // USE_OPENCV
+#include <string>
+#include <vector>
+
+#ifdef _MSC_VER
+
+#ifdef EXPORT_API
+#define __declspec(dllexport)   
+#else
+#define __declspec(dllimport) 
+#endif   
+#else
+#define DLL_API
+#endif
+
+namespace caffe
+{
+	using std::string;
+	/* Pair (label, confidence) representing a prediction. */
+	typedef std::pair<string, float> Prediction;
+
+	class Predictor {
+	public:
+		Predictor(const string& model_file,
+			const string& trained_file,
+			const string& mean_file,
+			const string& label_file);
+		~Predictor();
+
+		std::vector<Prediction> Classify(const cv::Mat& img, int N = 5);
+		bool Predict(const cv::Mat& img);
+		const std::vector<int>& getInputBlobShape();
+		bool getBlobs(const std::vector<std::string>& blobNames, std::vector< std::vector<float> >& blobdata, std::vector< std::vector<int> >& blobshape);
+		void getBlobNames(std::vector<std::string>& blobNames);
+
+		static bool setDevice(const int device_id);
+	private:
+		void SetMean(const string& mean_file);
+		void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+		void Preprocess(const cv::Mat& img,
+			std::vector<cv::Mat>* input_channels);
+
+	private:
+		void * net_;// shared_ptr<Net<float> > net_;
+		cv::Size input_geometry_;
+		int num_channels_;
+		cv::Mat mean_;
+		std::vector<string> labels_;
+	};
+}
+
---
+++ src/caffe/test/test_caffeapi.cpp
@@ -0,0 +1,77 @@
+// TestCaffe.cpp : Defines the entry point for the console application.
+//
+
+#include <caffe/prediction.hpp>
+#include <opencv2/opencv.hpp>
+#include <Windows.h>
+#include <iostream>
+#include <iomanip>
+using namespace caffe;
+using namespace std;
+using namespace cv;
+
+long long milliseconds_now() {
+	static LARGE_INTEGER s_frequency;
+	static BOOL s_use_qpc = QueryPerformanceFrequency(&s_frequency);
+	if (s_use_qpc) {
+		LARGE_INTEGER now;
+		QueryPerformanceCounter(&now);
+		return (1000LL * now.QuadPart) / s_frequency.QuadPart;
+	}
+	else {
+		return GetTickCount();
+	}
+}
+
+int main() //_tmain(int argc, _TCHAR* argv[])
+{
+	string modelpath = "models\\bvlc_reference_caffenet";
+	//string modelpath = "E:\\code\\xiaoice\\ImageML\\private\\Src\\XiaoiceSense\\ObjectDetection\\Model\\Temp\\vggmodel";
+	//string modelpath = "E:\\code\\xiaoice\\ImageML\\private\\Src\\XiaoiceSense\\ObjectDetection\\Model\\Temp\\model_500";
+	//string modelpath = "E:\\code\\xiaoice\\ImageML\\private\\Src\\XiaoiceSense\\ObjectDetection\\Model\\Temp\\models_VGGNet_coco_SSD_300x300\\models\\VGGNet\\coco\\SSD_300x300";
+	string model_proto = modelpath + "\\deploy.prototxt";
+	string model_weight = modelpath + "\\model.caffemodel";
+	string mean_file = "";
+	string label_file = modelpath + "\\synset_words.txt";
+	//string imagefile = "E:\\code\\xiaoice\\ImageML\\private\\Src\\XiaoiceSense\\Apps\\FaceCameraV2\\CameraCapture.Virtual\\data\\frame.jpg";
+	string imagefile = "examples\\images\\cat.jpg";
+	Mat img = imread(imagefile);
+	vector<string> blobnames;
+	vector<vector<float>> blobdata;
+	vector<vector<int>> blobshapes;
+	Predictor caffert(model_proto, model_weight, mean_file, label_file);
+	vector<int> caffeshape = caffert.getInputBlobShape();
+	resize(img, img, Size(caffeshape[2], caffeshape[3]));
+	//while (true)
+	//{
+	//	long long start = milliseconds_now();
+	//	bool result = caffert.Predict(img);
+	//	long long elapsed = milliseconds_now() - start;
+	//	cout << "SSD cost time : " << elapsed << endl;
+	//	if (result)
+	//	{
+	//		caffert.Classify(img, 5);
+	//		//caffert.getBlobNames(blobnames);
+	//		//caffert.getBlobs(blobnames, blobdata, blobshapes);	
+
+	//		//cout << "Blob names:" <<endl;
+	//		//for (int i = 0; i < blobnames.size(); i++)
+	//		//	cout << blobnames[i] << endl;
+
+	//	}
+	//}
+
+	vector<Prediction> predictions = caffert.Classify(img);
+
+	/* Print the top N predictions. */
+	for (size_t i = 0; i < predictions.size(); ++i) {
+		Prediction p = predictions[i];
+		cout << fixed << setprecision(4) << p.second << " - \""
+			<< p.first << "\"" << endl;
+	}
+
+		
+	return 0;
+}
+
+
